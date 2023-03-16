a01cb1b376a7207583272f08438148799d2cf90d
Locurador <patriciomeraz@gmail.com>
2023-03-16 09:10:33 -0500

    End Chapter 5

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -27,8 +27,8 @@ export class AccountService {
   }
 
   register(model: any) {
-    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
-      map(user => {
+    return this.http.post<User>(this.baseUrl + 'account/register', model)
+      .pipe(map(user => {
         if (user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
